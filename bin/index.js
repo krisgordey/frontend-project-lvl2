d8a69e4b81cc8cc302ac41efae2a9453dadb2e18
Kristina Gordeeva <krisgordeeva@gmail.com>
2020-11-09 19:06:22 +0300

    add format option, description

--- bin/index.js
+++ bin/index.js
@@ -7,5 +7,12 @@ const { version } = JSON.parse(fs.readFileSync('package.json', 'utf8'))
 const program = new commander.Command();
 
 program.version(version)
+  .arguments('<filepath1> <filepath2>')
+  .description('Compares two configuration files and shows a difference.')
+
+program
+    .option('-f, --format [type]', 'output format')
 
 program.parse(process.argv);
+
+if (program.format) console.log(`- ${program.format}`);
