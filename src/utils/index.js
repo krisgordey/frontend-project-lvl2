f99b94fd45c7cee9d7087c00d223ebdedcb3a08b
Kristina Gordeeva <krisgordeeva@gmail.com>
2020-11-22 23:11:20 +0300

    fix eslint errors

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,9 +1,10 @@
 import fs from 'fs';
 import path from 'path';
 
+// eslint-disable-next-line import/prefer-default-export
 export const readFile = (filePath) => {
   const fullPath = path.resolve(process.cwd(), filePath);
   const data = fs.readFileSync(fullPath).toString();
 
   return data;
-}
+};
