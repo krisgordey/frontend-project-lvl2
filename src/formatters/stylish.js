bbcb27bcdfa8c560b55c73c3f4650ce633fdd07c
Kristina Gordeeva <krisgordeeva@gmail.com>
2020-12-16 10:16:53 +0300

    Fixes part 4

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,12 +1,6 @@
 import _ from 'lodash';
 import Types from '../utils/constants.js';
 
-const Marker = {
-  MINUS: '-',
-  PLUS: '+',
-  EMPTY: ' ',
-};
-
 const markerOffset = 4;
 const firstIndent = 2;
 const getIndent = (depth) => (' ').repeat(firstIndent + markerOffset * depth);
@@ -26,15 +20,15 @@ const format = (diffTree) => {
   const iter = (tree, depth) => tree.map((node) => {
     switch (node.type) {
       case Types.NESTED:
-        return `${getIndent(depth)}${Marker.EMPTY} ${node.key}: {\n${iter(node.children, depth + 1)}\n${getIndent(depth)}  }`;
+        return `${getIndent(depth)}  ${node.key}: {\n${iter(node.children, depth + 1)}\n${getIndent(depth)}  }`;
       case Types.ADDED:
-        return `${getIndent(depth)}${Marker.PLUS} ${node.key}: ${stringify(node.value, depth)}`;
+        return `${getIndent(depth)}+ ${node.key}: ${stringify(node.value, depth)}`;
       case Types.DELETED:
-        return `${getIndent(depth)}${Marker.MINUS} ${node.key}: ${stringify(node.value, depth)}`;
+        return `${getIndent(depth)}- ${node.key}: ${stringify(node.value, depth)}`;
       case Types.CHANGED:
-        return `${getIndent(depth)}${Marker.MINUS} ${node.key}: ${stringify(node.oldValue, depth)}\n${getIndent(depth)}${Marker.PLUS} ${node.key}: ${stringify(node.newValue, depth)}`;
+        return `${getIndent(depth)}- ${node.key}: ${stringify(node.oldValue, depth)}\n${getIndent(depth)}+ ${node.key}: ${stringify(node.newValue, depth)}`;
       case Types.UNCHANGED:
-        return `${getIndent(depth)}${Marker.EMPTY} ${node.key}: ${stringify(node.value, depth)}`;
+        return `${getIndent(depth)}  ${node.key}: ${stringify(node.value, depth)}`;
       default:
         throw new Error(`unexpected node type: ${node.type}`);
     }
